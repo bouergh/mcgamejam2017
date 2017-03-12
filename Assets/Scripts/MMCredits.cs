e4badbd55ec61879517408a10ff23ab59f3637a6
bouergh <voodbutcher@gmail.com>
2017-03-12 13:41:06 -0400

    credits added, need to fix background for end credits

--- Assets/Scripts/MMCredits.cs
+++ Assets/Scripts/MMCredits.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class MMCredits : MonoBehaviour {
 
+    [SerializeField]
+    private Animator anim;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -13,7 +16,11 @@ public class MMCredits : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         if (Input.GetButton("Item")) SceneManager.LoadScene("MainMenu");
-	}
+        if (Input.GetButton("Glasses")) anim.SetTrigger("3D");
+        if (Input.GetKey("left")) anim.SetTrigger("Left");
+        if (Input.GetKey("right")) anim.SetTrigger("Right");
+
+    }
     
 
     public void BackToMenu(string nomScene)
