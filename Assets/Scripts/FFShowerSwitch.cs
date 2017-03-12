1a766c3ba7932257dda08da405a48cd16392556f
FrFerry <francois.ferry@polymtl.ca>
2017-03-12 11:56:48 -0400

    corrections des sons

--- Assets/Scripts/FFShowerSwitch.cs
+++ Assets/Scripts/FFShowerSwitch.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class FFShowerSwitch : MonoBehaviour
 {
     private bool switchable;
-    private bool isON;
-    private bool displayLabel;
+  //  private bool displayLabel;
     bool lockedInRoutine;
     private IEnumerator coroutine;
     [SerializeField]
@@ -17,8 +16,10 @@ public class FFShowerSwitch : MonoBehaviour
     private Sprite sprite1;
     [SerializeField]
     private Sprite sprite2;
-
     [SerializeField]
+    private float tempsReactivationDouche;
+
+     [SerializeField]
     private AudioClip SHOWER;
     [SerializeField]
     private AudioClip SHOWER_OFF;
@@ -28,8 +29,7 @@ public class FFShowerSwitch : MonoBehaviour
     void Start()
     {
         switchable = false;
-        displayLabel = false;
-        isON = true;
+       // displayLabel = false;
     }
 
     // Update is called once per frame
@@ -51,36 +51,58 @@ public class FFShowerSwitch : MonoBehaviour
     {
         if (switchable && GameObject.FindGameObjectWithTag("Player").GetComponent<FFPlayerController>().realWorld)
         {
+            Debug.Log("switch off the shower");
+            trapAssociated.GetComponent<FFTrapBehaviour>().Desactivate();
+            gameObject.GetComponent<SpriteRenderer>().sprite = sprite2;
+            GetComponents<AudioSource>()[1].Stop();
+            GetComponents<AudioSource>()[2].Stop();
+            GetComponent<AudioSource>().PlayOneShot(SHOWER_OFF);
+            yield return new WaitForSeconds(tempsReactivationDouche);
+            Debug.Log("switch on the shower");
+            trapAssociated.GetComponent<FFTrapBehaviour>().Activate();
+            gameObject.GetComponent<SpriteRenderer>().sprite = sprite1;
+            GetComponent<AudioSource>().PlayOneShot(SHOWER_OFF);
+            GetComponents<AudioSource>()[1].Play();
+            GetComponents<AudioSource>()[2].Play();
+            lockedInRoutine = false;
+        }
+    }
+    /*
+    private IEnumerator SwitchShower()
+    {
+        if (switchable && GameObject.FindGameObjectWithTag("Player").GetComponent<FFPlayerController>().realWorld)
+        {
             if (isON)
             {
                 Debug.Log("switch off the shower");
                 trapAssociated.GetComponent<FFTrapBehaviour>().Desactivate();
                 gameObject.GetComponent<SpriteRenderer>().sprite = sprite2;
                 isON = false;
-				GetComponents<AudioSource>()[1].Stop();
-				GetComponents<AudioSource>()[2].Stop();
-				GetComponent<AudioSource>().PlayOneShot(SHOWER_OFF);
-            } else
+                GetComponents<AudioSource>()[1].Stop();
+                GetComponents<AudioSource>()[2].Stop();
+                GetComponent<AudioSource>().PlayOneShot(SHOWER_OFF);
+            }
+            else
             {
                 Debug.Log("switch on the shower");
                 trapAssociated.GetComponent<FFTrapBehaviour>().Activate();
                 gameObject.GetComponent<SpriteRenderer>().sprite = sprite1;
                 isON = true;
                 GetComponent<AudioSource>().PlayOneShot(SHOWER_OFF);
-				GetComponents<AudioSource>()[1].Play();
-				GetComponents<AudioSource>()[2].Play();
-			}
+                GetComponents<AudioSource>()[1].Play();
+                GetComponents<AudioSource>()[2].Play();
+            }
         }
         yield return new WaitForSeconds(1f);
         lockedInRoutine = false;
     }
-
+    */
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            displayLabel = true;
+         //   displayLabel = true;
             switchable = true;
         }
     }
@@ -89,17 +111,17 @@ public class FFShowerSwitch : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            displayLabel = false;
+            //displayLabel = false;
             switchable = false;
         }
     }
 
-    private void OnGUI()
+   /* private void OnGUI()
     {
         if (displayLabel && GameObject.Find("Player").GetComponent<FFPlayerController>().realWorld)
         {
             Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
             GUI.Label(new Rect(pos.x, pos.y, Screen.width, Screen.height), "Press [CTRL]");
         }
-    }
+    }*/
 }
